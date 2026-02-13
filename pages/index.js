803851a3cf1770b308592646de2c5f47229bddd3
iisnapff-star <iisnapff@gmail.com>
2026-02-13 20:44:53 +0300

    add home page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>Volunteer System</h1>
+      <p>النظام يعمل بنجاح 🔥</p>
+    </div>
+  );
+}
